9aa5dd6a1b2300da766923bf3c41f844f686a6c4
andela <bizip04@gmail.com>
2020-12-10 23:31:27 +0200

    ft(Landing page): responsive landing page
    -Make footer responsive
    Add responsivenes to body
    Use icons for social media
    
    [ done ]

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer=()=>{
     <div className="flex-1 max-w-8xl max-auto p-10">
     <ul className="md:grid sm:grid-flow-col mt-30 grid-col-1 gap-4 md:grid-cols-2 md:gap-8 grid-flow-col-dense sm:auto-cols-min mb-2">
     <li className="rounded-lg shadow-xl flex-col md:p-12"><div className="max-h-full">
-    <h1 className="text-gray-200 font-bold text-2xl font-Sansation">BareFoot</h1><p className="max-h-full p-1 pt-5 mb-2  text-gray-200 font-bold md:text-xl font-Sansation">Bare foot is atravela nd accomodattion website for nomads we make your travel experience more woenderful with our incomperable services.</p>
+    <h1 className="text-gray-200 font-bold text-2xl font-Sansation">BareFoot</h1><p className="max-h-full p-1 pt-5 mb-2  text-gray-200 font-bold md:text-xl font-Sansation">Barefoot is a grobal travel and accomodation website for nomads we make your travel experience more wonderful with our incomperable services.</p>
     </div></li>
     <li className="rounded-lg shadow-xl md:mr-12 md:p-12"><div className="max-h-full">
     <h1 className="text-gray-200 font-bold text-2xl font-Sansation">Contact</h1><p className="text-gray-200 pt-5 font-bold md:text-xl font-Sansation">Barefoot customer services<br /> +250785855328<br />barefoot@gmail.com <br /><i class="fab fa-whatsapp"></i></p>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,9 +7,9 @@ const landingPage = () => {
     <div className='w-screen overflow-hidden'>
       <Navigation />
       <LandingSection />
-      <div className='py-10 px-40 font-Sansation text-2xl'>
+      <div className='py-10 md:px-40 px-10 font-Sansation text-2xl'>
         <p className='font-semibold'>Welcome to Barefoot nomad.</p>
-        <p>An application that enables everyone to book their international travel and accommodation globally; easily and conveniently across all the location where we operate. To continue you need to sign in if you have an account on the website or sign up to create an account.</p>
+        <p>An application that enables everyone to book their international travel and accommodation globally, easily and conveniently across all the location where we operate. To continue you need to sign in if you have an account on the website or sign up to create an account.</p>
       </div>
     </div>
   )
