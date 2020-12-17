47350fbd3d10c1162743a5beebbff074449c7f54
UMUHIRE Heritier <you@example.com>
2020-12-17 11:15:38 +0200

    Feature to login to the system by: - providing email  - Password  - by social media  - on verfied user should login  - messages displayed at success or at fail

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,16 @@ module.exports = {
     "verbose": true,
     "roots": [
       "./src/test"
-    ],
-    "transform": {
-      "^.+\\.js?$": "babel-jest"
+    ],"transform": {
+      '^.+\\.jsx?$': 'babel-jest',
+      '\\.(jpg|jpeg|png|css|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>src/config/assetsTransformer.js',
     },
     "setupFiles": [
       "./src/setupTests"
-    ]
+    ],"moduleNameMapper": {
+      "^.*(jpe?g|png|css).*$": "<rootDir>src/config/assetsTransformer.js"
+    },
+    "testPathIgnorePatterns": ['\\\\node_modules\\\\'],
+    "transformIgnorePatterns": ['<rootDir>/node_modules/']
   }
